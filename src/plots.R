210fff779b11b5ca52a5341218303c6fd634044b
JoshuaDanielRubin <jdrubin@middlebury.edu>
2023-07-12 15:02:43 +0200

    Add files

--- src/plots.R
+++ src/plots.R
@@ -1,24 +1,29 @@
 #!/usr/bin/env Rscript
 
-requiredPackages = c('ggplot2')
-suppressMessages(
-for(p in requiredPackages){
-  if(!require(p,character.only = TRUE)) install.packages(p)
-  library(p,character.only = TRUE, quietly = TRUE)
-})
+# Load required packages or install them if not present
+requiredPackages <- c('ggplot2')
+for (p in requiredPackages) {
+  if (!require(p, character.only = TRUE)) install.packages(p)
+  library(p, character.only = TRUE, quietly = TRUE)
+}
 
-args <- (commandArgs(TRUE))
+# Get arguments from command line
+args <- commandArgs(TRUE)
+name_bam <- args[1]
 directory <- args[2]
 
-name_bam <- args[1]
+# Set file paths
+all_info_path <- file.path(directory, "all_info.csv")
+conditions_rc_path <- file.path(directory, "conditions_rc.csv")
+pdf_file1 <- file.path(directory, paste0(name_bam, "_Rplots2.pdf"))
+pdf_file2 <- file.path(directory, paste0(name_bam, "_Rplots3.pdf"))
 
+# Load data
+df <- read.csv(all_info_path)
+data <- read.csv(conditions_rc_path)
 
-df <- read.csv(paste(directory, "all_info.csv", sep = ""))
-
-file_directory = paste(directory,name_bam, sep="")
-pdf(file=paste(file_directory, "_Rplots2.pdf", sep= ""))
-
-#Plot of p_est and r_fixed against GC content
+# Plot p_est and r_fixed against GC content
+pdf(pdf_file1)
 par(mar = c(5, 4, 4, 4) + 0.3)  # Leave space for z axis
 plot(df$GC_bins, df$p_estimated, xlab="GC content bins", ylab="P estimated") # first plot
 par(new = TRUE)
@@ -28,21 +33,14 @@ mtext("R fixed", side=4, line=3)
 
 # Plot lambda with error bars
 plot(df$GC_bins, df$Lambda, xlab="GC content bins", ylab="Predicted read counts", pch=20, cex=2)
-# Add error bars
 arrows(x0=df$GC_bins, y0=df$Lambda-df$Standard_deviations, x1=df$GC_bins, y1=df$Lambda+df$Standard_deviations, code=3, angle=90, length=0.1)
+dev.off()
 
-notprint<-dev.off()
-
-################################################################
-
-data <- read.csv(paste(directory, "conditions_rc.csv", sep = ""))
-
-plot_list = list()
-for(i in 3:ncol(data)) {
-  #variable <-names(data)[x+2]
-  #print(predicted)
-  condition <-names(data[, i])
-  plot<-ggplot(data, aes(x=rau)) +
+# Loop over conditions to generate plots
+plot_list <- list()
+for (i in 3:ncol(data)) {
+  condition <- names(data[, i])
+  plot <- ggplot(data, aes(x=rau)) +
     geom_point(aes(y=Observed_rc), col="blue") +
     geom_line(aes(y=unlist(data[ ,i])), col="red") +
     scale_y_continuous(name="Observed", sec.axis=sec_axis(~./1, name="Predicted")) +
@@ -54,12 +52,10 @@ for(i in 3:ncol(data)) {
       axis.text.y.right=element_text(color="red")
     )
   plot_list[[i]] <- plot
-
 }
 
+# Save plots to PDF
+pdf(pdf_file2)
+lapply(plot_list, print)
+dev.off()
 
-pdf(file=paste(file_directory, "_Rplots3.pdf", sep= ""))
-for(i in 3:ncol(data)) {
-    print(plot_list[[i]])
-}
-not_print<-dev.off()
